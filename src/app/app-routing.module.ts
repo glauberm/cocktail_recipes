e120e527e000701341b07235d1993730d2144dc1
Glauber Mota <glaubernm@gmail.com>
2018-12-07 18:36:58 -0200

    Fix route flow

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DrinkSearchComponent } from './drink-search/drink-search.component';
 import { DrinkDetailComponent } from './drink-detail/drink-detail.component';
 
 const routes: Routes = [
-  { path: '', component: DrinkSearchComponent },
   { path: 'drink/:id', component: DrinkDetailComponent },
 ];
 
